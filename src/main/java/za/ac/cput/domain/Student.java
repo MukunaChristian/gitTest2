690915bf3dbe94d932fc03235bffd088df1a18cb
Dell <christianmbuyimukuna@gmail.com>
2023-03-08 14:27:17 +0200

    this is the new student class with source

---
+++ src/main/java/za/ac/cput/domain/Student.java
@@ -0,0 +1,20 @@
+package za.ac.cput.domain;
+
+public class Student {
+
+    private String name;
+    private String lastName;
+    private int StudentNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getStudentNumber() {
+        return StudentNumber;
+    }
+}
